00c69398c9977e4454fb7e79847c01fe7ebe800e
Mike Rossetti <s2t2@users.noreply.github.com>
2020-06-02 23:54:02 -0400

    Setup development database

--- README.md
+++ README.md
@@ -14,6 +14,14 @@ cd twitoff-pt5/
 pipenv install
 ```
 
+Migrate the database:
+
+```sh
+FLASK_APP=web_app flask db init
+FLASK_APP=web_app flask db migrate
+FLASK_APP=web_app flask db upgrade
+```
+
 # Usage
 
 ```sh
--- web_app/__init__.py
+++ web_app/__init__.py
@@ -2,13 +2,22 @@
 
 from flask import Flask
 
+from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
 
+DATABASE_URI = "sqlite:///twitoff_development_pt5.db" # using relative filepath
+
 def create_app():
     app = Flask(__name__)
+
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    db.init_app(app)
+    migrate.init_app(app, db)
+
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
+
     return app
 
 if __name__ == "__main__":
