14c23704a91c5cd635b9e3fbfbd45194df265276
Mike Rossetti <s2t2@users.noreply.github.com>
2020-06-03 00:01:57 -0400

    Interface with database

--- web_app/routes/book_routes.py
+++ web_app/routes/book_routes.py
@@ -2,24 +2,32 @@
 
 from flask import Blueprint, jsonify, request, render_template #, flash, redirect
 
+from web_app.models import db, Book, parse_records
+
 book_routes = Blueprint("book_routes", __name__)
 
 @book_routes.route("/books.json")
 def list_books():
-    books = [
-        {"id": 1, "title": "Book 1"},
-        {"id": 2, "title": "Book 2"},
-        {"id": 3, "title": "Book 3"},
-    ]
+    #books = [
+    #    {"id": 1, "title": "Book 1"},
+    #    {"id": 2, "title": "Book 2"},
+    #    {"id": 3, "title": "Book 3"},
+    #]
+    book_records = Book.query.all()
+    print(book_records)
+    books = parse_records(book_records)
     return jsonify(books)
 
 @book_routes.route("/books")
 def list_books_for_humans():
-    books = [
-        {"id": 1, "title": "Book 1"},
-        {"id": 2, "title": "Book 2"},
-        {"id": 3, "title": "Book 3"},
-    ]
+    #books = [
+    #    {"id": 1, "title": "Book 1"},
+    #    {"id": 2, "title": "Book 2"},
+    #    {"id": 3, "title": "Book 3"},
+    #]
+    book_records = Book.query.all()
+    print(book_records)
+    books = parse_records(book_records)
     return render_template("books.html", message="Here's some books", books=books)
 
 @book_routes.route("/books/new")
@@ -29,9 +37,13 @@ def new_book():
 @book_routes.route("/books/create", methods=["POST"])
 def create_book():
     print("FORM DATA:", dict(request.form))
-    # todo: store in database
+
+    new_book = Book(title=request.form["book_title"], author_id=request.form["author_name"])
+    db.session.add(new_book)
+    db.session.commit()
+
     return jsonify({
-        "message": "BOOK CREATED OK (TODO)",
+        "message": "BOOK CREATED OK",
         "book": dict(request.form)
     })
     #flash(f"Book '{new_book.title}' created successfully!", "success")
