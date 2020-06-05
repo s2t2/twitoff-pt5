f2a6e002ed285c4112ce1298144294b3a3909554
Mike Rossetti <s2t2@users.noreply.github.com>
2020-06-04 23:50:20 -0400

    Store tweets and users in the database

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -5,6 +5,7 @@ from flask import Flask
 from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
+from web_app.routes.twitter_routes import twitter_routes
 
 DATABASE_URI = "sqlite:///twitoff_development_pt5.db" # using relative filepath
 
@@ -17,6 +18,7 @@ def create_app():
 
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
+    app.register_blueprint(twitter_routes)
 
     return app
 
