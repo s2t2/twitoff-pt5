26be4cbdee80dd656f4e20a010252967fdcc19d6
Mike Rossetti <s2t2@users.noreply.github.com>
2020-06-08 23:06:16 -0400

    Navigation for our stats routes

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -6,6 +6,7 @@ from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
 from web_app.routes.book_routes import book_routes
 from web_app.routes.twitter_routes import twitter_routes
+from web_app.routes.stats_routes import stats_routes
 
 DATABASE_URI = "sqlite:///twitoff_development_pt5.db" # using relative filepath
 
@@ -19,6 +20,7 @@ def create_app():
     app.register_blueprint(home_routes)
     app.register_blueprint(book_routes)
     app.register_blueprint(twitter_routes)
+    app.register_blueprint(stats_routes)
 
     return app
 
