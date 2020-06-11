036ffa978008355aab3fcee91330b254b253dd75
Mike Rossetti <s2t2@users.noreply.github.com>
2020-06-10 22:53:08 -0400

    Read db connection creds from env var

--- web_app/__init__.py
+++ web_app/__init__.py
@@ -1,6 +1,10 @@
 # web_app/__init__.py
 
+import os
 from flask import Flask
+from dotenv import load_dotenv
+
+load_dotenv() # loads env vars from the .env file (locally)
 
 from web_app.models import db, migrate
 from web_app.routes.home_routes import home_routes
@@ -8,14 +12,14 @@ from web_app.routes.book_routes import book_routes
 from web_app.routes.twitter_routes import twitter_routes
 from web_app.routes.stats_routes import stats_routes
 
-DATABASE_URI = "sqlite:///twitoff_development_pt5.db" # using relative filepath
+DATABASE_URL = os.getenv("DATABASE_URL")
 SECRET_KEY = "temporary secret value. todo read from env var and customize on production to keep sessions secure"
 
 def create_app():
     app = Flask(__name__)
     app.config["SECRET_KEY"] = SECRET_KEY
 
-    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URI
+    app.config["SQLALCHEMY_DATABASE_URI"] = DATABASE_URL
     db.init_app(app)
     migrate.init_app(app, db)
 
